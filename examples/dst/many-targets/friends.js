b38c4a0d52607b6f80e7f3248cbbf180fab22d2c
Vladimir Tsvang <vtsvang@gmail.com>
2013-04-23 22:06:22 +0400

    remove examples/dst folder

--- examples/dst/many-targets/friends.js
+++
@@ -1,4 +0,0 @@
-define(['dust-runtime'], function () {
-	// src/friends.dust
-	(function(){dust.register("src/friends.dust",body_0);function body_0(chk,ctx){return chk.section(ctx.get("friends"),ctx,{"block":body_1},null);}function body_1(chk,ctx){return chk.reference(ctx.get("name"),ctx,"h").write(", ").reference(ctx.get("age"),ctx,"h").write("\n");}return body_0;})();
-});
--- examples/dst/many-targets/nested/inline-params.js
+++
@@ -1,4 +0,0 @@
-define(['dust-runtime'], function () {
-	// src/nested/inline-params.dust
-	(function(){dust.register("src/nested/inline-params.dust",body_0);function body_0(chk,ctx){return chk.section(ctx.get("profile"),ctx,{"block":body_1},{"bar":"baz","bing":"bong"});}function body_1(chk,ctx){return chk.reference(ctx.get("name"),ctx,"h").write(", ").reference(ctx.get("bar"),ctx,"h").write(", ").reference(ctx.get("bing"),ctx,"h");}return body_0;})();
-});
